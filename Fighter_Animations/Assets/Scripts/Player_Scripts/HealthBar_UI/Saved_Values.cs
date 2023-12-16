48cded8b4be4922616a2435ee68c98d507f4d85e
velt1c <143429150+velt1c@users.noreply.github.com>
2023-12-15 19:00:48 -0500

    Revert "Revert "Added the timer as well as the health bars""

---
+++ Fighter_Animations/Assets/Scripts/Player_Scripts/HealthBar_UI/Saved_Values.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saved_Values : MonoBehaviour
+{
+    public static float Player1Health = 1.0f;
+    public static float Player2Health = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
